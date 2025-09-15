2d64c94cdc777444f84c64b0fdf295f105bd61b8
JohannHicbol <johannhichcasbolanos@gmail.com>
2025-09-15 00:41:31 -0400

    Password gen.py

--- Password gen.py
+++ Password gen.py
@@ -2,9 +2,23 @@ import random
 
 char = "abcdefghijlompqrstuvwxyz1234567890ABCDEFGSIJKLOIMPQRSTUVWXYZ!@#$%^&*()"
 
+def get_password_len():
+    while True:
+        try:
+            return int(input("how long would you like your password to be?:"))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+def get_password_count():
+    while True:
+        try:
+            return int(input("how many passwords would you like?:"))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 while 1:
-    password_len= int(input("how long would you like your password to be?:"))
-    password_count = int(input("how many passwords would you like?:"))
+    password_len= get_password_len()
+    password_count = get_password_count()
     for x in range(0,password_count):
        password = ""
        for x in range(0, password_len):
